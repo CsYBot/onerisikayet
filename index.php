45e458293a0bcc967ba0fad8a9d34e2406d2443d
CsYBot <67687653+CsYBot@users.noreply.github.com>
2021-01-28 22:16:24 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+//-----------------------------------------------------------------
+/*
+$verialmakicin = "Beklemede";
+
+    $sorgu = $db->prepare("SELECT * FROM siparis WHERE durum=:duruma Order By tarih DESC");
+    $sorgu->execute([
+
+":duruma" => $verialmakicin
+
+
+    ]);
+
+    while ($cikti = $sorgu->fetch(PDO::FETCH_ASSOC)) {
+echo "test: ".$cikti['iletisim']. "";
+$varmi = "var";
+    }
+ */
+
+
+
+//------------------------------------------------------
+						
+
+
+/*
+$kontrola = $db->prepare("SELECT * FROM test1 WHERE test2=?");
+
+$kontrola->execute(array($test)); 
+if($kontrola->rowCount()){  
+//---------------------------------------------------------------------------
+
+$sorgu = $db->prepare("insert into linkler (test) values ('$test')");
+
+$sorgu->execute();
+----------------------------------------------------------------------------
+
+
+
+$sorgu = $db->query("select * from siparis where kod like '%$getir%'",PDO::FETCH_ASSOC);
+    $sorgu->execute();
+
+    while ($cikti = $sorgu->fetch(PDO::FETCH_ASSOC)) {
+        echo "".$cikti['iletisim']. "";
+
+*/
+?>
