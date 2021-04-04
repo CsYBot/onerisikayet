b7cf49b62900e9217d72aaf0f6c50ff93651a1b7
CsYBot <67687653+CsYBot@users.noreply.github.com>
2021-04-04 22:08:43 +0300

    Update index.php

--- index.php
+++ index.php
@@ -34,6 +34,26 @@ $sorgu = $db->prepare("insert into linkler (test) values ('$test')");
 
 $sorgu->execute();
 
+//---------------------------------------------------------------------------
+session_start();
+ob_start();
+if(!isset($_SESSION["login"])){
+
+header("Location: login.php"); 
+
+} else {
+
+echo "adminsin";
+
+}
+
+//$_SESSION["login"] = "true";
+//$_SESSION["user"] = user;
+//session_destroy();
+//ob_end_flush();
+
+
+
 
 //---------------------------------------------------------------------------
 
